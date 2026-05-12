83394afedd2df1716f40dbf97b43f12ed2328824
Xusanboy <tursunovkhusanboy@gmail.com>
2026-05-12 16:09:40 +0500

    remove theme

--- lib/main.dart
+++ lib/main.dart
@@ -18,21 +18,6 @@ class VoiceRecorderDemoApp extends StatelessWidget {
       dependencies: dependencies,
       child: MaterialApp(
         title: 'Voice Chat',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(
-            seedColor: Colors.deepPurple,
-            brightness: Brightness.light,
-          ),
-        ),
-        darkTheme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(
-            seedColor: Colors.deepPurple,
-            brightness: Brightness.dark,
-          ),
-        ),
         home: const ChatPage(),
       ),
     );
